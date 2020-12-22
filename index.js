9639e7166335dd5e5bb7211de28c8c0b2f686a77
fatima-dah <dahnini.fatima@hotmail.fr>
2020-12-22 11:52:40 +0100

    finish quest

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const connection = require("./connection");
 
 const app = express();
-const port = 8000;
+const port = 3000;
 
 connection.connect(function(err) {
   if (err) {
